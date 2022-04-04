672780d51eb11291039f34a60615c3d6aa81757e
Michał M. Sapka <michal@sapka.me>
2022-04-04 12:49:12 +0200

    Some more

---
+++ autostart.sh
@@ -0,0 +1,4 @@
+/opt/homebrew/bin/dmenu-mac &
+brew services restart yabai
+brew services restart skhd
+
---
+++ nvim/init.lua
@@ -0,0 +1,9 @@
+# General options
+vim.opt.backup = false
+vim.opt.swapfile = false
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true
