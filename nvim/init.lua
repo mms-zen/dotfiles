1d3ec936a1dbf774ec1f3ffc08f6fe712c72e68e
Michał M. Sapka <michal@sapka.me>
2022-05-02 15:42:55 +0200

    Move to Dracula theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,13 +73,14 @@ return packer.startup(function(use)
 	use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 	use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 	use "overcache/NeoSolarized" -- Solarized theme for NeoVim
+	use "Mofiqul/dracula.nvim" -- Dracula theme
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
 		require("packer").sync()
 	end
-	vim.cmd('colorscheme NeoSolarized')
+	vim.cmd('colorscheme dracula')
 end)
 
 
