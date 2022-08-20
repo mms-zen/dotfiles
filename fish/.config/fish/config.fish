d7f05c3b53dd8e64357701264b1fece6d7ed4fe2
Michal Sapka <michal@sapka.me>
2022-08-20 22:32:27 +0200

    feature: fish aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,24 @@
+set fish_greeting ""
+
+# nvim for all
+command -qv nvim && alias vim nvim && alias vi nvim
+
+# git aliases as muscle memory die hard
+alias ga "git add"
+alias gaa "git add ."
+alias gc "git commit"
+alias gp "git push"
+alias gst "git status"
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
 source /opt/asdf-vm/asdf.fish
+
+
