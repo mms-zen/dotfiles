45107f6eb9fb95f15355e749ebe6a2a475866c8e
Michał M. Sapka <michal@sapka.me>
2022-08-26 17:41:09 +0200

    feat: fish treesitter

--- nvim/.config/nvim/lua/user/treesitter.lua
+++ nvim/.config/nvim/lua/user/treesitter.lua
@@ -4,7 +4,7 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = {"html", "ruby", "go", "javascript", "bash", "java", "json", "tsx", "lua", "norg" },
+  ensure_installed = {"html", "ruby", "go", "javascript", "bash", "java", "json", "tsx", "lua", "norg", "fish" },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
