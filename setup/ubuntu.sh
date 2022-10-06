110fc30ab31e2d08a0467bf7bc7cf1ecc7f3d331
Michał Sapka <michal@sapka.me>
2022-10-06 23:08:42 +0200

    feat: use PPa for nvim

--- setup/ubuntu.sh
+++ setup/ubuntu.sh
@@ -9,6 +9,11 @@ flatpak install \
 	app/com.valvesoftware.Steam/x86_64/stable \
 	-y
 
+type -p curl >/dev/null || sudo apt install curl -y
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo add-apt-repository ppa:neovim-ppa/stable
 
@@ -28,6 +33,7 @@ sudo apt install \
 	starship \
 	tldr \
   neovim \
+  gh \
 	-y
 
 curl -sS https://starship.rs/install.sh | sh
